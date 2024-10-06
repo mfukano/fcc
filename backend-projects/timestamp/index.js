e92cec9462eead7a4ebbc2105b009b1a253f3742
Mat Fukano <fukano.mat@gmail.com>
2024-10-06 03:41:56 -0700

    Add request logging

--- backend-projects/timestamp/index.js
+++ backend-projects/timestamp/index.js
@@ -7,6 +7,10 @@ var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 app.use(express.static("public"));
+app.use((req, res, next) => {
+  console.log(`-- ${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
