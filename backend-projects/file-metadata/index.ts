8b38281b0da50f5adeb9c2ed36c1028163769dc0
Mat Fukano <fukano.mat@gmail.com>
2024-10-17 23:26:48 -0700

    Handle CORS headers differently

--- backend-projects/file-metadata/index.ts
+++ backend-projects/file-metadata/index.ts
@@ -1,16 +1,21 @@
 const REC_FILES_PATH = `./received_files`;
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
 console.log("Hello via Bun!");
 const server = Bun.serve({
   port: 4000,
   async fetch(req) {
     const path = new URL(req.url).pathname;
 
-    if (path === "/")
-      return new Response(Bun.file("index.html"), {
-        headers: {
-          "Content-Type": "text/html",
-        },
-      });
+    if (req.method === "OPTIONS") {
+      return new Response(null, { status: 204, headers: CORS_HEADERS });
+    }
+
+    if (path === "/") return new Response(Bun.file("index.html"));
 
     if (path === "/api/upload") {
       const formData = await req.formData();
@@ -24,11 +29,19 @@ const server = Bun.serve({
       const writtenFile = Bun.file(`${REC_FILES_PATH}/${filename}`);
       const writtenFileBytes = await writtenFile.bytes();
 
-      return Response.json({
-        name: writtenFile.name,
-        size: writtenFileBytes.length,
-        type: writtenFile.type,
-      });
+      return new Response(
+        JSON.stringify({
+          name: writtenFile.name,
+          size: writtenFileBytes.length,
+          type: writtenFile.type,
+        }),
+        {
+          headers: {
+            ...CORS_HEADERS,
+            "Content-Type": "application/json",
+          },
+        },
+      );
     }
 
     if (path === "/cleanup") {
